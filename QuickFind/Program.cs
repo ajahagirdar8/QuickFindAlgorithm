84dee8b3ee0e1f1689ab84eee15555442e91bb1d
Amruta Jahagirdar <ajahagirdar8@gmail.com>
2020-08-24 07:19:26 +0530

    first commit

---
+++ QuickFind/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace QuickFind
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Enter size of the array:");
+			string s = Console.ReadLine ();
+			int n = int.Parse (s);
+
+			QuickFind qf = new QuickFind (n);
+
+			qf.Connect (2, 3);
+
+			bool isConnected = qf.isConnected (2, 3);
+
+			Console.WriteLine ("2 and 3 :" + isConnected);
+
+			isConnected = qf.isConnected (2, 5);
+
+			Console.WriteLine ("2 and 5 :" + isConnected);
+
+			qf.Connect (2, 5);
+
+			isConnected = qf.isConnected (2, 5);
+			Console.WriteLine ("2 and 5 :" + isConnected);
+
+			isConnected = qf.isConnected (3, 5);
+
+			Console.WriteLine ("3 and 5 :" + isConnected);
+
+		}
+	}
+}
---
+++ QuickFind/QuickFind.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace QuickFind
+{
+	public class QuickFind
+	{
+		private int[] ID;
+
+		public QuickFind (int N)
+		{
+			ID = new int[N];
+			for(int i=0; i < N; i++) {
+
+				ID [i] = i;
+			}
+		}
+
+		public bool isConnected(int p, int q)
+		{
+			return ID [p] == ID [q];
+		}
+
+		public void Connect(int p, int q)
+		{
+			if(!isConnected(p,q))
+				{
+					int valAtP = ID [p];
+					int valAtQ = ID [q];
+
+					for(int i =0;i< ID.Length;i++)
+					{
+					if (ID [i] == valAtP)
+						ID [i] = valAtQ;
+					}
+				}
+
+		}
+	}
+}
+
